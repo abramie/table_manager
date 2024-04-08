31b6ab566cb756d0f151c2375a08368ce6eb11a2
abramie <jeremyrou@hotmail.fr>
2024-04-08 18:49:47 +0200

    Tentative de reparation bug sqlite

--- database/migrations/2024_08_04_create_jeux_table.php
+++ database/migrations/2024_08_04_create_jeux_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('jeux');
         Schema::create('jeux', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
