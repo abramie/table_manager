d77302d1e96261b534f0bd0e95f214922e1002b8
abramie <jeremyrou@hotmail.fr>
2025-04-17 19:12:15 +0200

    Squelette changement mails

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Triggerwarning;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TestController extends Controller
+{
+    //
+
+    public function add(){
+
+    }
+}
