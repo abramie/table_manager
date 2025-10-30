5a2b2cb2f2aa8e12bbf390ebde1aa862378de3db
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2025-10-30 15:37:37 +0100

    feat: Ajouts tables pour les logs. Parce que clairement c'est la priorité là maintenant ....

---
+++ app/Models/Log.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\types\TypeLog;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Auth;
+
+class Log extends Model
+{
+    //
+    public function loggable() :  MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function type_log() : BelongsTo{
+        return $this->belongsTo(TypeLog::class);
+    }
+    public function profil(): BelongsTo{
+        return $this->belongsTo(Profile::class);
+    }
+
+    /**
+     * @param Profile|null $profile : Le profil actif associé au log (par default l'utilisateur connecté)
+     * @param $code : Le code du Log pour l'identifier
+     * @param $detail : Information completementaire (optionnel)
+     * @param $save : Si le log est sauvegarder dans la base de donnée ou non
+     * @return Log
+     */
+    static public function log(Profile|null $profile = null, $code = 'DEFAULT', Tag|Triggerwarning|Table|null $objet = null, $detail = "", $save = true){
+
+        $log = new Log();
+        $log->details = $detail;
+        if($profile){
+            $log->profil()->associate($profile);
+        }else{
+            $log->profil()->associate(Auth::user()->currentProfile());
+        }
+        $log->loggable()->associate($objet);
+        $log->type_log()->associate(TypeLog::findCode($code));
+        //Ajouter type log
+        if($save)$log->save();
+        return $log;
+
+    }
+}
