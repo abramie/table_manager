5d4b29dcd0fba52fcae889657937f0138df34d91
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2026-03-16 16:17:21 +0100

    fix: theme edition de compte

--- resources/views/compte/edit.blade.php
+++ resources/views/compte/edit.blade.php
@@ -16,19 +16,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-">
                 <div class="max-w-xl">
                     @include('compte.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 ">
                 <div class="max-w-xl">
                     @include('compte.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8">
                 <div class="max-w-xl">
                     @include('compte.partials.delete-user-form')
                 </div>
