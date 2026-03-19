cf86c6c67fe0361a534e61c12c48e6be1ebec8c3
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2026-03-19 08:11:49 +0100

    feat: theme footer

--- resources/views/layouts/foot.blade.php
+++ resources/views/layouts/foot.blade.php
@@ -1,8 +1,8 @@
-<footer  class="bottom text-center">
-    <div>Si vous rencontrez un bug, ou que vous avez une idée d'amelioration rendez vous sur :
+<footer  class="bottom text-center footer">
+    <div class="footer-inner">Si vous rencontrez un bug, ou que vous avez une idée d'amelioration rendez vous sur :
         <a  class="link-danger" href="https://github.com/abramie/table_manager/issues" target="_blank">La page github, pour ajouter un ticket</a>
     </div>
-    <div>
+    <div class="footer-inner">
         Il s'agit d'une version beta, normalement tous fonctionne correctement, mais le site est toujours en developpement et des bugs peuvent s'y etre glisser.
         Pensez à signaler les choses, et j'essaierais de mettre à jour au plus vite.
     </div>
