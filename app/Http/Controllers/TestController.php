3f325ac65ab4ce75de70892c1cfe40b3292dbdfd
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2026-03-19 16:18:48 +0100

    test: envoie email

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,7 +15,7 @@ class TestController extends Controller
 
     public function mail(){
 
-        dd(Mail::to(Auth::user())->send(new MailTest()));
+        dd(Mail::to(Auth::user()->email)->send(new MailTest()));
         return "ok mail envoyer";
     }
     public function add(){
