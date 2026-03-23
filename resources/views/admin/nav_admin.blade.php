ea194f85d23fafca6f83de414bf1c7e806da65f1
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2026-03-23 15:05:24 +0100

    feat: theme navigation admin

--- resources/views/admin/nav_admin.blade.php
+++ resources/views/admin/nav_admin.blade.php
@@ -1,12 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <x-navigation-item routeName="admin.index">Options admins</x-navigation-item>
-            <x-navigation-item routeName="admin.users">Utilisateurs</x-navigation-item>
-            <x-navigation-item routeName="admin.settings">Settings</x-navigation-item>
-            <x-navigation-item routeName="admin.deleted" contain="deleted">Gestion des elements supprimés
-            </x-navigation-item>
 
-        </ul>
-    </div>
-</nav>
+<ul class="navbar-nav navbar-inner col">
+    <x-navigation-item routeName="admin.index">Options admins</x-navigation-item>
+    <x-navigation-item routeName="admin.users">Utilisateurs</x-navigation-item>
+    <x-navigation-item routeName="admin.settings">Settings</x-navigation-item>
+    <x-navigation-item routeName="admin.deleted" contain="deleted">Gestion des elements supprimés
+    </x-navigation-item>
+
+</ul>
+
--- resources/views/compte/base.blade.php
+++ resources/views/compte/base.blade.php
@@ -7,13 +7,13 @@
         $currentProfile = $authCompte?->currentProfile;
     @endphp
     <ul class="navbar-nav navbar-inner col">
-    <x-navigation-item routeName="compte.edit" contain="compte" :parameter="['compte' => $authCompte]" >Edit</x-navigation-item>
-    @role('joueur')
-        <x-navigation-item routeName="profile.show" :parameter="[$authCompte]"  >Profiles</x-navigation-item>
-    @endrole
-    @if($compte->hasProfile())
-        <x-navigation-item routeName="profile.mj" :parameter="['compte' => $authCompte, 'profile' => $currentProfile]"  >MJ</x-navigation-item>
-    @endif
+        <x-navigation-item routeName="compte.edit" contain="compte" :parameter="['compte' => $authCompte]" >Edit</x-navigation-item>
+        @role('joueur')
+            <x-navigation-item routeName="profile.show" :parameter="[$authCompte]"  >Profiles</x-navigation-item>
+        @endrole
+        @if($compte->hasProfile())
+            <x-navigation-item routeName="profile.mj" :parameter="['compte' => $authCompte, 'profile' => $currentProfile]"  >MJ</x-navigation-item>
+        @endif
     </ul>
 
 @endsection
