82783819381c620cdabfdc5276d1db6ef0f1fd17
jrouss31 <jeremy.rousseau@univ-poitiers.fr>
2026-03-23 14:57:46 +0100

    feat: Modification cards profiles

--- resources/views/compte/profile/index.blade.php
+++ resources/views/compte/profile/index.blade.php
@@ -6,38 +6,54 @@
 @section('content-compte')
     <h1>Profiles</h1>
 
-    <div class="d-flex flex-wrap">
+    <div class="d-flex ">
 
         @foreach($profiles as $profile)
 
-            <div class="card text-center @if($compte->currentProfile()->select('name')->first()->name == $profile->name) border-success @endif ">
+
 
                 <form method="POST" action="{{route('profile.update', [$compte, $profile])}}"
                       enctype="multipart/form-data">
                     @csrf
+                    <div class="card w-200 text-center me-2 @if($compte->currentProfile()->select('name')->first()->name == $profile->name) border-success @endif ">
+                        <div class="card-title">
+                            {{$profile->name}}
+                        </div>
+                        <div class="card-body p-2">
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="name">Pseudo*</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="name" value="{{$profile->name}}" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="email">Email</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="email" value="{{$profile->email}}" class="form-control">
+                                </div>
+                            </div>
 
-                    <div class="card-body">
-                        <h5 >
-                            <label class="label-text" for="name">Pseudo*</label>
-                            <input type="text" name="name" value="{{$profile->name}}" class="form-control">
-                        </h5>
-                        <label class="label-text" for="email">Email</label>
-                        <input type="text" name="email" value="{{$profile->email}}" class="form-control">
-                        <label class="label-text" for="discord_tag">Tag discord</label>
-                        <input type="text" name="discord_tag" value="{{$profile->discord_tag}}" class="form-control">
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="discord_tag">Tag discord</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="discord_tag" value="{{$profile->discord_tag}}" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <button class="btn btn-primary mt-2" type="submit">Mettre à jour</button>
+                            </div>
 
-                        <button class="btn btn-primary mt-2" type="submit">Mettre à jour</button>
 
-                    </div>
+                        </div>
 
-                    <div class="card-footer" >
-                        @if($compte->currentProfile()->select('name')->first()->name != $profile->name)
-                        <a href="{{route('profile.change', [$compte,$profile])}}" class="btn btn-validate">Rendre actif</a>
-                        <a href="{{route('profile.delete', [$compte,$profile])}}" class="btn btn-warning">Supprimer</a>
-                        @else
-                            <h2 class="profile-card-actif-title">Actif</h2>--}}
-                        @endif
-                    </div>
+                        <div class="card-footer" >
+                            @if($compte->currentProfile()->select('name')->first()->name != $profile->name)
+                            <a href="{{route('profile.change', [$compte,$profile])}}" class="btn btn-validate">Rendre actif</a>
+                            <a href="{{route('profile.delete', [$compte,$profile])}}" class="btn btn-warning">Supprimer</a>
+                            @else
+                                <h2 class="profile-card-actif-title">Actif</h2>
+                            @endif
+                        </div>
 
 {{--                    @if($compte->currentProfile()->select('name')->first()->name == $profile->name)--}}
 {{--                        <div class="" >--}}
@@ -46,31 +62,48 @@
 
 {{--                        </div>--}}
 {{--                    @endif--}}
+                    </div>
                 </form>
 
 
-            </div>
+
 
         @endforeach
         <form method="POST" action="{{route('profile.store', [$compte])}}" enctype="multipart/form-data">
             @csrf
-            <div class="card text-center">
+            <div class="card w-200 text-center me-2">
                 <div class="card-title">
                     Ajouter un profil
                 </div>
-                <div class="card-body">
-                    <h5 >
-                        <label class="label-text" for="name">Pseudo</label>
-                        <input name="name" type="text" value="" class="form-control">
-                    </h5>
-                    <label class="label-text" for="email">Email</label>
-                    <input name="email" type="text" value="" class="form-control">
-                    <label class="label-text" for="discord_tag">Tag discord</label>
-                    <input name="discord_tag" type="text" value="" class="form-control">
-                    <button class="btn btn-primary mt-2" type="submit">Ajouter le profil</button>
-                </div>
+                <div class="card-body p-2">
+                    <div class="row mb-3 me-2">
+                        <label class="label-text col-sm col-form-label" for="name">Pseudo*</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="name" value="{{old('name')}}" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row mb-3 me-2">
+                        <label class="label-text col-sm col-form-label" for="email">Email</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="email" value="{{old('email')}}" class="form-control">
+                        </div>
+                    </div>
 
+                    <div class="row mb-3 me-2">
+                        <label class="label-text col-sm col-form-label" for="discord_tag">Tag discord</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="discord_tag" value="{{old('discord_tag')}}" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <button class="btn btn-primary mt-2" type="submit">Ajouter le profil</button>
+                    </div>
+
+
+                </div>
             </div>
+
+
         </form>
     </div>
 
