15f59ab84588015a57369babe83a28d979f00bf9
abramie <26021013+abramie@users.noreply.github.com>
2026-03-25 19:55:19 +0100

    feat : css inscription unlogged

--- resources/views/table/inscription_unlogged.blade.php
+++ resources/views/table/inscription_unlogged.blade.php
@@ -7,150 +7,176 @@
 @endphp
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
+    <div class="d-flex ">
                 <!-- Session Status -->
-                <x-auth-session-status class="mb-4" :status="session('status')" />
+{{--                <x-auth-session-status class="mb-4" :status="session('status')" />--}}
 
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionLogin', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                     @csrf
-
+                    <div class="card w-200 text-center p-4 me-2  ">
+                        <div class="card-title">
+                            Se connecter
+                        </div>
                     <!-- Email Address -->
-                    <div class="form-group mb-3">
-                        <x-input-label for="email" :value="__('Email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
+                        <div class="card-body p-2">
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="email">{{__('Email')}}</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="email" value="{{old('email')}}" required autofocus autocomplete="email" class="form-control">
+                                </div>
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
 
                     <!-- Password -->
-                    <div class="mt-4 form-group">
-                        <x-input-label for="password" :value="__('Password')" />
-                        @if(config('app.env') !=  'local')
-                            <x-text-input id="password" class="block mt-1 w-full"
-                                          type="password"
-                                          name="password"
+                            @if(config('app.env') !=  'local')
+                                <div class="row mb-3 me-2">
+                                    <x-input-label for="password" :value="__('Password')" />
 
-                                          required
-                                          autocomplete="current-password" />
-                        @endif
+                                            <x-text-input id="password" class="block mt-1 w-full"
+                                                          type="password"
+                                                          name="password"
 
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                    </div>
+                                                          required
+                                                          autocomplete="current-password" />
 
-                    <!-- Remember Me -->
-                    <div class="block mt-4 form-group">
-                        <label for="remember_me" class="inline-flex items-center">
-                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                        </label>
-                    </div>
 
-                    <div class="form-group row align-items-end">
-                        <div  class="col">
-                            <x-primary-button class="btn btn-dark btn-xs pull-left" type="button" onclick="window.location='{{ url()->previous() }}'">
-                                Retour
-                            </x-primary-button>
-                        </div>
-                        @if (Route::has('password.request'))
-                            <div  class="col">
-                                <a class="text-center underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                                    {{ __('Forgot your password?') }}
-                                </a>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                </div>
+                            @endif
+
+
+
+                            <!-- Remember Me -->
+
+                            <div class="row mb-3 me-2">
+                                <label for="remember_me" class="inline-flex items-center">
+                                    <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                </label>
                             </div>
-                        @endif
-                        <div  class="col align-self-end">
-                            <x-primary-button >
-                                {{ __('Log in') }}
-                            </x-primary-button>
+
+
+                            <div class="form-group row align-items-end">
+                                <div  class="col">
+                                    <x-primary-button class="btn btn-dark btn-xs pull-left" type="button" onclick="window.location='{{ url()->previous() }}'">
+                                        Retour
+                                    </x-primary-button>
+                                </div>
+                                @if (Route::has('password.request'))
+                                    <div  class="col">
+                                        <a class="text-center underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                            {{ __('Forgot your password?') }}
+                                        </a>
+                                    </div>
+                                @endif
+                                <div  class="col align-self-end">
+                                    <x-primary-button >
+                                        {{ __('Log in') }}
+                                    </x-primary-button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </form>
-            </div>
-            <div class="col-sm">
+
+
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionRegister', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                     @csrf
+                    <div class="card w-200 text-center p-4 me-2  ">
 
-                    <!-- Name -->
-                    <div>
-                        <x-input-label for="name" :value="__('Pseudo')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                    </div>
+                        <!-- Name -->
+                        <div class="card-body p-2">
+                            <div class="row mb-3 me-2">
+                                <x-input-label for="name" :value="__('Pseudo')" />
+                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
 
-                    <!-- Email Address -->
-                    <div class="mt-4">
-                        <x-input-label for="email" :value="__('Email')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
+                                <label class="label-text col-sm col-form-label" for="name">{{__('Pseudo')}}</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="name" value="{{old('name')}}" required autofocus autocomplete="username" class="form-control">
+                                </div>
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
 
-                    <!-- Password -->
-                    <div class="mt-4">
-                        <x-input-label for="password" :value="__('Password')" />
 
-                        <x-text-input id="password" class="block mt-1 w-full"
-                                      type="password"
-                                      name="password"
-                                      required autocomplete="new-password" />
+                            <!-- Email Address -->
 
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                    </div>
+                            <div class="row mb-3 me-2">
+                                <x-input-label for="email" :value="__('Email')" />
+                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
 
-                    <!-- Confirm Password -->
-                    <div class="mt-4">
-                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                      type="password"
-                                      name="password_confirmation" required autocomplete="new-password" />
+                            <!-- Password -->
+                            <div class="mt-4">
+                                <x-input-label for="password" :value="__('Password')" />
 
-                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                    </div>
+                                <x-text-input id="password" class="block mt-1 w-full"
+                                              type="password"
+                                              name="password"
+                                              required autocomplete="new-password" />
 
-                    <div class="form-group row align-items-end">
-                        <div  class="col">
-                            <x-secondary-button class="btn ms-4" type="button" onclick="window.location='{{ url()->previous() }}'">
-                                Retour
-                            </x-secondary-button>
-                        </div>
-                        <div  class="col">
-                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                                {{ __('Already registered?') }}
-                            </a>
-                        </div>
-                        <div  class="col">
-                            <x-primary-button class="ms-4">
-                                {{ __('Register') }}
-                            </x-primary-button>
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+
+                            <!-- Confirm Password -->
+                            <div class="mt-4">
+                                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                              type="password"
+                                              name="password_confirmation" required autocomplete="new-password" />
+
+                                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                            </div>
+
+                            <div class="form-group row align-items-end">
+                                <div  class="col">
+                                    <x-secondary-button class="btn ms-4" type="button" onclick="window.location='{{ url()->previous() }}'">
+                                        Retour
+                                    </x-secondary-button>
+                                </div>
+                                <div  class="col">
+                                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                                        {{ __('Already registered?') }}
+                                    </a>
+                                </div>
+                                <div  class="col">
+                                    <x-primary-button class="ms-4">
+                                        {{ __('Register') }}
+                                    </x-primary-button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </form>
-            </div>
-            <div class="col-sm">
+
+
 
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionProfil', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                 @csrf
-                    <!-- Name -->
-                    <div>
-                        <x-input-label for="name" :value="__('Pseudo')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                    </div>
+                    <div class="card w-200 text-center p-4 me-2  ">
+                        <!-- Name -->
+                        <div>
+                            <x-input-label for="name" :value="__('Pseudo')" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
 
-                    <!-- Email Address -->
-                    <div class="mt-4">
-                        <x-input-label for="email" :value="__('Email (Optionnel)')" />
-                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" autocomplete="username" />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                    </div>
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-input-label for="email" :value="__('Email (Optionnel)')" />
+                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
 
-                    <x-primary-button class="ms-4">
-                        {{ __('Register') }}
-                    </x-primary-button>
+                        <x-primary-button class="ms-4">
+                            {{ __('Register') }}
+                        </x-primary-button>
+                    </div>
                 </form>
-            </div>
-        </div>
+
     </div>
 
 
--- resources/views/table/show.blade.php
+++ resources/views/table/show.blade.php
@@ -6,75 +6,77 @@
     $isSansTable = $table->sans_table == 1;
 @endphp
 @section('content')
-    <h1>{{ $table->nom}}</h1>
-    <p>
-        <button class="btn btn-xs btn-info " type="button"
-                onclick="window.location='{{ route("events.one.creneau.tablesindex",['evenement'=> $evenement,'creneau' =>$creneau]) }}'">
-            Retour à la liste des tables
-        </button>
-    </p>
-    @if(!$isSansTable)
-        <h5 class="card-subtitle mb-2 text-body-secondary">
-            MJ : {{$table->mjs->name}}
-
-        </h5>
-
-        @if(!$table->tags->isEmpty())
-            <p>
-            Tags :
-            @foreach($table->tags as $tag )
-                <span class="{{$tag->typeTag->bs_class}}">{{$tag->name}}</span>
-
-            @endforeach
-            </p>
-        @endif
-
-    @endif
-    <p>
-        @if(auth()->user() && (auth()->user()?->can('manage_tables_all') ||(auth()->user()?->can('manage_tables_own')&&  $table->mjs->name ==auth()->user()->mainProfile->name) ))
-            <button class="btn btn-xs btn-info pull-right" type="button"
-                    onclick="window.location='{{ route("events.one.creneau.table.edit",['evenement'=> $evenement,'creneau' =>$creneau, 'table'=>$table]) }}'">
-                Edit
+    <div class="table-info">
+        <h1>{{ $table->nom}}</h1>
+        <p>
+            <button class="som-btn btn-xs som-btn-secondary " type="button"
+                    onclick="window.location='{{ route("events.one.creneau.tablesindex",['evenement'=> $evenement,'creneau' =>$creneau]) }}'">
+                Retour à la liste des tables
             </button>
-            <button class="btn btn-xs btn-danger pull-right" type="button"
-                    onclick="window.location='{{ route("events.one.creneau.table.delete",['evenement'=> $evenement,'creneau' =>$creneau, 'table'=>$table]) }}'">
-                Supprimer
-            </button>
+        </p>
+        @if(!$isSansTable)
+            <h3 class=" mj">
+                MJ : {{$table->mjs->name}}
+
+            </h3>
+
+            @if(!$table->tags->isEmpty())
+                <p>
+                Tags :
+                @foreach($table->tags as $tag )
+                    <span class="{{$tag->typeTag->bs_class}}">{{$tag->name}}</span>
+
+                @endforeach
+                </p>
+            @endif
+
         @endif
-    </p>
-    <div>
-        Horaires : {{$table->debut_table->toTimeString($unitPrecision ='minute')}}
-        -> {{$table->debut_table->addHour($table->duree)->toTimeString($unitPrecision ='minute')}};
-    </div>
-    <div>
-        <h2>Description de la table</h2>
-        {{$table->description}}
-    </div>
-    @if($table->jeu)
+        <p>
+            @if(auth()->user() && (auth()->user()?->can('manage_tables_all') ||(auth()->user()?->can('manage_tables_own')&&  $table->mjs->name ==auth()->user()->mainProfile->name) ))
+                <button class="btn btn-xs btn-info pull-right" type="button"
+                        onclick="window.location='{{ route("events.one.creneau.table.edit",['evenement'=> $evenement,'creneau' =>$creneau, 'table'=>$table]) }}'">
+                    Edit
+                </button>
+                <button class="btn btn-xs btn-danger pull-right" type="button"
+                        onclick="window.location='{{ route("events.one.creneau.table.delete",['evenement'=> $evenement,'creneau' =>$creneau, 'table'=>$table]) }}'">
+                    Supprimer
+                </button>
+            @endif
+        </p>
         <div>
-            <h2>Le jeu : {{$table->jeu->nom}}</h2>
-            {{$table->jeu->description}}
+            Horaires : {{$table->debut_table->toTimeString($unitPrecision ='minute')}}
+            -> {{$table->debut_table->addHour($table->duree)->toTimeString($unitPrecision ='minute')}};
         </div>
-    @endif
+        <div>
+            <h2>Description de la table</h2>
+            {{$table->description}}
+        </div>
+        @if($table->jeu)
+            <div>
+                <h2>Le jeu : {{$table->jeu->nom}}</h2>
+                {{$table->jeu->description}}
+            </div>
+        @endif
 
-    <div>
-        <span>Inscrits : {{$table->nb_inscrits()}} @if(!$isSansTable)
-                / {{$table->nb_joueur_max}}
-            @endif</span>
+        <div>
+            <span>Inscrits : {{$table->nb_inscrits()}} @if(!$isSansTable)
+                    / {{$table->nb_joueur_max}}
+                @endif</span>
 
-        <ul class="list-group list-group-flush">
-            @foreach($table->inscriptions as $inscrit)
-                <li class="list-group-item {{$inscrit->type_inscription->bs_class}}">{{$inscrit->profile->name}}</li>
-            @endforeach
-        </ul>
+            <ul class="list-group list-group-flush">
+                @foreach($table->inscriptions as $inscrit)
+                    <li class="list-group-item {{$inscrit->type_inscription->bs_class}}">{{$inscrit->profile->name}}</li>
+                @endforeach
+            </ul>
+        </div>
+        <span>Nombre de pré-inscriptions possible : {{$table->max_preinscription}}</span>
+
+        @php
+            $ouverture_inscription =$evenement->ouverture_inscription;
+            $inscription_ouverte = $ouverture_inscription->isPast();
+            $fermeture_inscription =$evenement->fermeture_inscription;
+            $inscription_fermee = $fermeture_inscription->isPast();
+        @endphp
+        <x-bouton_inscription :table="$table"/>
     </div>
-    <span>Nombre de pré-inscriptions possible : {{$table->max_preinscription}}</span>
-
-    @php
-        $ouverture_inscription =$evenement->ouverture_inscription;
-        $inscription_ouverte = $ouverture_inscription->isPast();
-        $fermeture_inscription =$evenement->fermeture_inscription;
-        $inscription_fermee = $fermeture_inscription->isPast();
-    @endphp
-    <x-bouton_inscription :table="$table"/>
 @endsection
