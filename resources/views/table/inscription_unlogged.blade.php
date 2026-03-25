553e71894eeb7de4a88820347f6203185f6e34dc
abramie <26021013+abramie@users.noreply.github.com>
2026-03-25 20:31:55 +0100

    feat : css inscription unlogged

--- resources/views/table/inscription_unlogged.blade.php
+++ resources/views/table/inscription_unlogged.blade.php
@@ -7,16 +7,16 @@
 @endphp
 @section('content')
 
-    <div class="d-flex ">
+    <div class="d-flex flex-wrap">
                 <!-- Session Status -->
 {{--                <x-auth-session-status class="mb-4" :status="session('status')" />--}}
 
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionLogin', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                     @csrf
                     <div class="card w-200 text-center p-4 me-2  ">
-                        <div class="card-title">
+                        <h4 class="card-title mb-4">
                             Se connecter
-                        </div>
+                        </h4>
                     <!-- Email Address -->
                         <div class="card-body p-2">
                             <div class="row mb-3 me-2">
@@ -30,14 +30,16 @@
                     <!-- Password -->
                             @if(config('app.env') !=  'local')
                                 <div class="row mb-3 me-2">
-                                    <x-input-label for="password" :value="__('Password')" />
 
-                                            <x-text-input id="password" class="block mt-1 w-full"
-                                                          type="password"
-                                                          name="password"
+                                    <label class="label-text col-sm col-form-label" for="password">{{__('Password')}}</label>
+                                    <div class="col-sm-8">
+                                        <x-text-input id="password" class="block mt-1 w-full"
+                                          type="password"
+                                          name="password"
 
-                                                          required
-                                                          autocomplete="current-password" />
+                                          required
+                                          autocomplete="current-password" />
+                                    </div>
 
 
                                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -62,17 +64,16 @@
                                         Retour
                                     </x-primary-button>
                                 </div>
-                                @if (Route::has('password.request'))
-                                    <div  class="col">
-                                        <a class="text-center underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                                            {{ __('Forgot your password?') }}
-                                        </a>
-                                    </div>
-                                @endif
+
+                                <div  class="col-md-auto text-wrap">
+                                    <a class="text-center underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                </div>
                                 <div  class="col align-self-end">
-                                    <x-primary-button >
-                                        {{ __('Log in') }}
-                                    </x-primary-button>
+                                    <button class="ms-4 som-btn som-btn-validate">
+                                        {{ __('Se connecter et s\'inscrire') }}
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -83,59 +84,68 @@
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionRegister', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                     @csrf
                     <div class="card w-200 text-center p-4 me-2  ">
-
+                        <h4 class="card-title mb-4">
+                            Créer son compte
+                        </h4>
                         <!-- Name -->
                         <div class="card-body p-2">
                             <div class="row mb-3 me-2">
-                                <x-input-label for="name" :value="__('Pseudo')" />
-                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
 
                                 <label class="label-text col-sm col-form-label" for="name">{{__('Pseudo')}}</label>
                                 <div class="col-sm-8">
                                     <input type="text" name="name" value="{{old('name')}}" required autofocus autocomplete="username" class="form-control">
                                 </div>
-                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
 
 
                             <!-- Email Address -->
 
                             <div class="row mb-3 me-2">
-                                <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                <label class="label-text col-sm col-form-label" for="email">{{__('Email')}}</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="email" value="{{old('email')}}" required autofocus autocomplete="email" class="form-control">
+                                </div>
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
 
 
                             <!-- Password -->
-                            <div class="mt-4">
-                                <x-input-label for="password" :value="__('Password')" />
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="password">{{__('Password')}}</label>
+                                <div class="col-sm-8">
+                                    <x-text-input id="password" class="block mt-1 w-full"
+                                                  type="password"
+                                                  name="password"
 
-                                <x-text-input id="password" class="block mt-1 w-full"
-                                              type="password"
-                                              name="password"
-                                              required autocomplete="new-password" />
+                                                  required
+                                                  autocomplete="current-password" />
+                                </div>
+                            </div>
+
+                            <!-- Confirm Password -->
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="password_confirmation">{{__('Confirm Password')}}</label>
+                                <div class="col-sm-8">
+                                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                                  type="password"
+                                                  name="password_confirmation"
+
+                                                  required
+                                                  autocomplete="current-password" />
+                                </div>
 
                                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
 
-                            <!-- Confirm Password -->
-                            <div class="mt-4">
-                                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                              type="password"
-                                              name="password_confirmation" required autocomplete="new-password" />
-
-                                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                            </div>
 
                             <div class="form-group row align-items-end">
                                 <div  class="col">
-                                    <x-secondary-button class="btn ms-4" type="button" onclick="window.location='{{ url()->previous() }}'">
+                                    <button class="som-btn som-btn-secondary ms-4 " type="button" onclick="window.location='{{ url()->previous() }}'">
                                         Retour
-                                    </x-secondary-button>
+                                    </button>
                                 </div>
                                 <div  class="col">
                                     <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
@@ -143,9 +153,9 @@
                                     </a>
                                 </div>
                                 <div  class="col">
-                                    <x-primary-button class="ms-4">
+                                    <button class="ms-4 som-btn som-btn-validate">
                                         {{ __('Register') }}
-                                    </x-primary-button>
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -157,23 +167,34 @@
                 <form method="POST" action="{{ route('events.one.creneau.table.inscriptionProfil', ['evenement' => $evenement, 'creneau' => $creneau, "table" => $table]) }}">
                 @csrf
                     <div class="card w-200 text-center p-4 me-2  ">
-                        <!-- Name -->
-                        <div>
-                            <x-input-label for="name" :value="__('Pseudo')" />
-                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                        </div>
+                        <div class="card-body p-2">
+                            <h4 class="card-title mb-4">
+                                S'inscrire sans compte
+                            </h4>
+                            <!-- Name -->
+                            <div class="row mb-3 me-2">
 
-                        <!-- Email Address -->
-                        <div class="mt-4">
-                            <x-input-label for="email" :value="__('Email (Optionnel)')" />
-                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" autocomplete="username" />
-                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                        </div>
 
-                        <x-primary-button class="ms-4">
-                            {{ __('Register') }}
-                        </x-primary-button>
+                                <label class="label-text col-sm col-form-label" for="name">{{__('Pseudo')}}</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="name" value="{{old('name')}}" required autofocus autocomplete="username" class="form-control">
+                                </div>
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                            </div>
+
+                            <!-- Email Address -->
+                            <div class="row mb-3 me-2">
+                                <label class="label-text col-sm col-form-label" for="email">{{__('Email (Optionnel)')}}</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="email" value="{{old('email')}}"  autofocus autocomplete="email" class="form-control">
+                                </div>
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+
+                            <button class="ms-4 som-btn som-btn-validate">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
                     </div>
                 </form>
 
